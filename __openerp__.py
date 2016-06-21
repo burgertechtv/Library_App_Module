9141c1eafd229d16c76983d19162c9fec4321e00
Rob Burgertech <burgertechtv@gmail.com>
2016-06-21 11:43:27 +0200

    removed data file attribute that was unlinked

--- __openerp__.py
+++ __openerp__.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
 {
 	'name': "Library Module",
-	'summary': "Short subtitle phrase",
-	'description': """Long description""",
-	'author': "Your name",
-	'license': "AGPL-3",
-	'website': "http://www.example.com",
-	'category': 'Uncategorized',
-	'version': '9.0.1.0.0',
-	'depends': ['base'],
-	'data': ['views.xml'],
-	'demo': ['demo.xml'],
 }
