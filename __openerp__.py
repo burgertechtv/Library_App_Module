ea43271528e04b9ef28049e490c7cfab3af77676
Rob Burgertech <burgertechtv@gmail.com>
2016-06-21 11:51:18 +0200

    Added views folder and template view without data, just outline/shell

--- __openerp__.py
+++ __openerp__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-	'name': "Library Module",
+	'name': "Library Module V1.1.0",
+	'summary': "Manage your books",
+	'depends': ['base'],
+	'data': ['views/library_book.xml'],
 }
